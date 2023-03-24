be29ef0d54774572bf53053df24369a9eb8695cc
YGR-99 <115626550+YGR-99@users.noreply.github.com>
2023-03-24 13:00:03 +0100

    osm update

--- Yúri/osm_data.R
+++ Yúri/osm_data.R
@@ -1,10 +1,11 @@
 easypackages::packages("sf", "sp", "osmdata")
 
-read_sf("C:/Users/ygrin/Documents/GitHub/Noord_Holland_house_prices/Yúri/data/Noord_holland_polygon.shp") %>%
-  st_transform(crs=4326) %>%
-  st_write("Noord_Holland.gpkg", )
+#read_sf("data/Noord_holland_polygon.shp") %>%
+ # st_transform(crs=4326) %>%
+  #st_write("Noord_Holland.gpkg", )
 
-province_boundaries <- st_read ("C:/Users/ygrin/Documents/GitHub/Noord_Holland_house_prices/Yúri/data/Noord_Holland.gpkg")
+province_boundaries <- st_read ("data/Noord_Holland.gpkg")
+funda_data <- st_read ("data/test_sample(100p).gpkg")
 
 # Get OSM data
 bus <-  opq(st_bbox(province_boundaries)) %>%
@@ -26,12 +27,10 @@ train_points <- st_as_sf (train$osm_points) %>% dplyr:: select(osm_id, amenity)
 
 ############################################################ DISTANCES ###########################################################
 #Re-project the point layer from WGS to local projection to have unit in meter
-bikefac_points <-  st_transform (bikefac_points, 27700)
+bus_points <-  st_transform (bus_points, 28992)
+subway_points <-  st_transform (subway_points, 28992)
+train_points <-  st_transform (train_points, 28992)
 
-#Re-project the point layer from WGS to local projection to have unit in meter
-shop_points <-  st_transform (Dailyshops_points, 27700)
-
-#now we would write our own function to estimate distance using nearest neighbor algorithm
 #This function take an origin point (from), and a destination point (to), and then use nn function of FNN package to estimate the distance to the nearest (k =1) neighborhood location
 nn_function <- function(measureFrom,measureTo,k) {
   measureFrom_Matrix <- as.matrix(measureFrom)
@@ -41,16 +40,14 @@ nn_function <- function(measureFrom,measureTo,k) {
   return(nn)
 }
 
-#now we will use the nn_function to measure the shortest distance from neighborhood centroid to  bike facilities (i.e., bikefac_points)
-#distance to bike facilities (shops, repair, parking)
-greendatacen <- greendatacen %>%
-  mutate(bikefac_dist_new = nn_function(st_coordinates(greendatacen$geom), st_coordinates(bikefac_points$geom), 1))
+#distance to bus stop
+funda_data <- funda_data %>%
+  mutate(bus_dist = nn_function(st_coordinates(funda_data$geom), st_coordinates(bus_points$geom), 1))
 
-#distance to shops
-greendatacen <- greendatacen %>%
-  mutate(Shop_dist_new = nn_function(st_coordinates(greendatacen$geom), st_coordinates(shop_points$geom), 1))
+#distance to subway entrance
+funda_data <- funda_data %>%
+  mutate(subway_dist = nn_function(st_coordinates(funda_data$geom), st_coordinates(subway_points$geom), 1))
 
-#you can see the data frame and there is new column at the end.
-#view(greendatacen)
-
-#this is how you can convert the raw OSM data into a spatial indicator
+#distance to train station
+funda_data <- funda_data %>%
+  mutate(train_dist = nn_function(st_coordinates(funda_data$geom), st_coordinates(train_points$geom), 1))
