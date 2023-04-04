9b26326d24dc8f91c1774b9a6474f48435fce9fc
JitseRuurd <jitseruurdn@gmail.com>
2023-04-04 13:56:08 +0200

    Update 3. Final analysis & Results.R

--- 3. Final analysis & Results.R
+++ 3. Final analysis & Results.R
@@ -1,8 +1,12 @@
-easypackages::packages("tidyverse", "sf", "mapview", "RColorBrewer", "tmap")
+easypackages::packages("tidyverse", "sf", "mapview", "RColorBrewer", "tmap", "spdep")
 
 
 #Visualize results GWR model
-gwr_result<- st_read("data/models/mgwr_results_amsterdam_ndvi.gpkg")
+gwr_result<- st_read("data/models/gwr_results_amsterdam_ndvi_m2.gpkg")
+funda_data <- st_read("data/Houseprices/funda_buy_amsterdam_31-03-2023_full_distances.gpkg")
+
+funda_data %>% 
+  mapview( zcol = "price_m2", col.regions=brewer.pal(9, "YlOrRd"), at = seq(4000, 25000, 1000))
 
 gwr_result %>% 
   filter(tram_dist_TV < -1.96| tram_dist_TV > 1.96) %>% 
@@ -16,16 +20,42 @@ gwr_result %>%
   filter(metro_dist_TV < -1.96| metro_dist_TV > 1.96) %>% 
   mapview( zcol = "metro_dist", col.regions=brewer.pal(9, "YlOrRd"))
 
-gwr_result %>% 
-  filter(living_area_TV < -1.96| living_area_TV > 1.96) %>% 
-  mapview( zcol = "living_area", col.regions=brewer.pal(9, "YlOrRd"))
 
 gwr_result %>% 
-  filter(ndvi100_TV < -1.96| ndvi100_TV > 1.96) %>% 
-  mapview( zcol = "ndvi100", col.regions=brewer.pal(9, "YlOrRd"))
+  filter(ndvi500_TV < -1.91| ndvi500_TV > 1.91) %>% 
+  mapview( zcol = "ndvi500", col.regions=brewer.pal(9, "YlOrRd"))
 
 
 
+gwr_result %>% 
+  filter(school_dist_TV < -1.91| school_dist_TV > 1.91) %>% 
+  mapview( zcol = "school_dist", col.regions=brewer.pal(9, "YlOrRd"))
+
+gwr_result %>% 
+  filter(shops_dist_TV < -1.91| shops_dist_TV > 1.91) %>% 
+  mapview( zcol = "shops_dist", col.regions=brewer.pal(9, "YlOrRd"))
+
+gwr_result %>% 
+  filter(centre_dist_TV < -1.91| centre_dist_TV > 1.91) %>% 
+  mapview( zcol = "centre_dist", col.regions=brewer.pal(9, "YlOrRd"))
+
+gwr_result %>% 
+  filter(zuid_dist_TV < -1.91| zuid_dist_TV > 1.91) %>% 
+  mapview( zcol = "zuid_dist", col.regions=brewer.pal(9, "YlOrRd"))
+
+gwr_result %>% 
+  mapview( zcol = "residual", col.regions=brewer.pal(9, "YlOrRd"))
+
+
+funda_KNN <- knearneigh(funda_data, k=5) #Identify k nearest neighbours for spatial weights 
+funda_nbq_KNN <- knn2nb(funda_KNN, sym=T) #Neighbours list from knn object
+funda_KNN_w <- nb2listw(funda_nbq_KNN, style="W", zero.policy = TRUE)
+
+mc_gwr <- moran.mc(gwr_result$residual, funda_KNN_w, 2999, zero.policy= TRUE, alternative="greater")
+mc_gwr
+plot(mc_gwr)
+funda_data %>% 
+  ggplot(aes(y = price_m2, x = metro_dist)) + geom_point()
 
 map <- mapview(gwr_result, zcol = "metro", col.regions=brewer.pal(9, "YlOrRd"))
 
