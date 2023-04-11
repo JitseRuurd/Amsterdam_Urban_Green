910a451a6165256873b1bd8c651766179b2c46af
thomasnib <117747807+thomasnib@users.noreply.github.com>
2023-04-11 11:49:48 +0200

    update

--- 3. Final analysis & Results.R
+++ 3. Final analysis & Results.R
@@ -32,4 +32,29 @@ tm_shape(PC4, bbox = bbox_new)+
             legend.text.size = 0.75,
             legend.title.size = 1)
 
+################## create coef plot for ndvi for paper
+paper_plot <- tm_shape(PC4, bbox = bbox_new) + 
+  tm_polygons(col = 'lightgrey') + 
+  tm_shape(gwr_result %>% filter(ndvi300_TV < -1.96 | ndvi300_TV > 1.96)) + 
+  tm_dots(col = 'ndvi300', 
+          size = 0.2,
+          midpoint = 0, 
+          title = "Residential Property Premiums (in euros)",
+          palette = 'Spectral',
+          shape = 21) + 
+  tm_layout(title.position = c('center', 'top'),
+            title.fontface = 'bold',
+            title.fontfamily = 'Times',
+            legend.title.fontface = 'bold',
+            legend.title.fontfamily = 'Times',
+            legend.text.fontfamily = 'Times',
+            legend.position = c(0.03, -0.01),
+            legend.text.size = 0.5,
+            legend.title.size = 0.9,
+            frame = F)
+
+paper_plot
+
+# Save
+tmap_save(paper_plot, 'Economic_Value_Urban_Green.png', width = 5, height = 3.5)
 
