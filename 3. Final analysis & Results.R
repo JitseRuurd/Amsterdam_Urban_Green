6e972f967ad2cc3163d1e96bc62e21ff1bb2cc6b
thomasnib <117747807+thomasnib@users.noreply.github.com>
2023-04-11 14:25:40 +0200

    update

--- 3. Final analysis & Results.R
+++ 3. Final analysis & Results.R
@@ -1,8 +1,9 @@
-easypackages::packages("tidyverse", "sf", "mapview", "RColorBrewer", "tmap", "spdep")
+easypackages::packages("tidyverse", "sf", "mapview", "RColorBrewer", "tmap", "spdep", 'stars')
 
 #Visualize results GWR model
 gwr_result<- st_read("data/models/gwr_results_amsterdam_ndvi300_m2.gpkg")
 PC4 <- st_transform(st_read("data/Amsterdam/PC4.json"), crs = 28992)
+ndvi <- read_stars("data/Greenness/NDVI_Amsterdam_300m.tif")
 
 #plot variables of interest
 # Extend bbox for plots
@@ -10,9 +11,9 @@ bbox_new <- st_bbox(PC4) # current bounding box
 xrange <- bbox_new$xmax - bbox_new$xmin # range of x values 
 yrange <- bbox_new$ymax - bbox_new$ymin # range of y values 
 # bbox_new[1] <- bbox_new[1] - (0.25 * xrange) # xmin - left 
-bbox_new[3] <- bbox_new[3] + (0.25 * xrange) # xmax - right 
+bbox_new[3] <- bbox_new[3] + (0 * xrange) # xmax - right 
 # bbox_new[2] <- bbox_new[2] - (0.25 * yrange) # ymin - bottom 
-bbox_new[4] <- bbox_new[4] + (0.25 * yrange) # ymax - top 
+bbox_new[4] <- bbox_new[4] + (0 * yrange) # ymax - top 
 bbox_new <- bbox_new %>%
   st_as_sfc() 
 
@@ -57,4 +58,19 @@ paper_plot
 
 # Save
 tmap_save(paper_plot, 'Economic_Value_Urban_Green.png', width = 5, height = 3.5)
+
+################## create plot for ndvi for paper
+tm_shape(ndvi, bbox = bbox_new)+
+  tm_raster(palette = "YlGn", title = "NDVI") +
+  tm_shape(PC4, bbox = bbox_new)+
+  tm_polygons(col = "white", alpha = 0, border.col = "black")+
+  tm_layout(title.fontfamily = "Times",
+            title.fontface = "bold",
+            legend.text.fontfamily = "Times",
+            legend.title.fontfamily = "Times",
+            legend.text.size = 0.75,
+            legend.title.size = 1,
+            frame = F,
+            legend.outside.position = c('right', 'center'),
+            legend.outside = T)
 
